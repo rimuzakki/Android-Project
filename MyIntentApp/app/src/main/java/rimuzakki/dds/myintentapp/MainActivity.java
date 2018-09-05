a42d1b4e12ac95135b1b6ccee9dc0966621622d9
rimuzakki <rimuzakki@gmail.com>
2018-09-05 19:43:37 +0700

    Initial commit

---
+++ MyIntentApp/app/src/main/java/rimuzakki/dds/myintentapp/MainActivity.java
@@ -0,0 +1,30 @@
+package rimuzakki.dds.myintentapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnMoveActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMoveActivity = (Button)findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+        }
+    }
+}
